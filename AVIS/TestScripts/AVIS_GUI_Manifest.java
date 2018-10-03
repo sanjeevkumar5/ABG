814754d0e8d0fb9d4d553045eb225ac7f60104e2
reddyara <reddyara@PACQ.asiapacific.hpqcorp.net>
2018-10-03 17:13:33 +0530

    Implement screenshot in the code

--- AVIS/TestScripts/AVIS_GUI_Manifest.java
+++ AVIS/TestScripts/AVIS_GUI_Manifest.java
@@ -1,6 +1,13 @@
-package Manifest_Avis;
+package Manifest_GUI;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -28,22 +35,22 @@ public class AVIS_GUI_Manifest {
 			ReadWriteExcel rwe = new ReadWriteExcel("C:\\Backup\\ABG SE Scripts\\TestData\\Manifest\\AVIS_GUITestData_Manifest_Regression.xlsx");
 			System.setProperty("webdriver.chrome.driver", "C:\\Backup\\ABG SE Scripts\\chromedriver.exe");
 			WebDriver driver = new ChromeDriver();
-				
-				String Execute 					= rwe.getCellData("INPUT_DATA", k, 	2);
-				String THINCLIENTURL 			= rwe.getCellData("INPUT_DATA", k, 	7);
-				String USERID 					= rwe.getCellData("INPUT_DATA", k, 	8);
-				String PASSWORD 				= rwe.getCellData("INPUT_DATA", k, 	9);
-				String OUTSTATION				= rwe.getCellData("INPUT_DATA", k, 10);
-//				String COUNTER_NUM				= rwe.getCellData("INPUT_DATA", k, 14);
+			String ScreenShotPath = "C:\\Selenium\\Screenshots\\Avis_Manifest\\";
 			
+				String Execute 					= rwe.getCellData("Manifest_Avis", k, 	2);
+				String TESTCASENAME 			= rwe.getCellData("Manifest_Avis", k, 	4);
+				String THINCLIENTURL 			= rwe.getCellData("Manifest_Avis", k, 	6);
+				String USERID 					= rwe.getCellData("Manifest_Avis", k, 	7);
+				String PASSWORD 				= rwe.getCellData("Manifest_Avis", k, 	8);
+				String OUTSTATION				= rwe.getCellData("Manifest_Avis", k,  	9);
+//				String COUNTER_NUM				= rwe.getCellData("Manifest_Avis", k,  13);
+				
 			if ( Execute.equals("N") )	
 				driver.close();
 			
 			if ( Execute.equals("Y") )
 			{
 				driver.get(THINCLIENTURL + OUTSTATION);
-				String tokenURL = rwe.getCellData("INPUT_DATA", k, 6);
-				System.out.println(tokenURL);			
 				driver.findElement(By.name("username")).sendKeys(USERID);
 				driver.findElement(By.name("PASSWORD")).sendKeys(PASSWORD);
 					
@@ -59,14 +66,14 @@ public class AVIS_GUI_Manifest {
 				driver.findElement(By.xpath("//*[@id=\"manifestToDate\"]")).clear();
 				Thread.sleep(2000);
 					
-				String OneDay_LOR = rwe.getCellData("INPUT_DATA", k, 12);
+				String OneDay_LOR = rwe.getCellData("Manifest_Avis", k, 11);
 				driver.findElement(By.xpath("//*[@id=\"manifestToDate\"]")).sendKeys(OneDay_LOR);
 				Thread.sleep(2000);
 							
 				driver.findElement(By.xpath("//*[@id=\"getManifest\"]")).click();
 				Thread.sleep(8000);
 				
-				String DetailType = rwe.getCellData("INPUT_DATA", k, 11);
+				String DetailType = rwe.getCellData("Manifest_Avis", k, 10);
 				System.out.println(DetailType);
 					
 			    driver.findElement(By.xpath("//*[@id=\"manifest_submenu\"]/div/div[3]/button")).click();;
@@ -84,6 +91,11 @@ public class AVIS_GUI_Manifest {
 	
 	// Logout the application when the "No reservations found"
 				if(NoResFound.equals(ResVal) || NoResFound.equals(NoData)){
+					Date d= new Date();
+					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+					File src= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+					FileUtils.copyFile(src,new File( ScreenShotPath +TESTCASENAME+ sdf.format(d)+"_1Day"+".pgn"));
+					
 					driver.findElement(By.id("headerLogOutButton")).click();
 					Thread.sleep(2000);
 					driver.findElement(By.id("logoutForm:yesLogoutButton")).click();
@@ -99,7 +111,7 @@ public class AVIS_GUI_Manifest {
 				 
 				    // remove the last character 
 				    str = str.substring(9, str.length()-5);
-				    rwe.setCellData("INPUT_DATA", k, 15, str);
+				    rwe.setCellData("Manifest_Avis", k, 14, str);
 					Thread.sleep(2000);
 			        		
 					driver.findElement(By.xpath("//div[@class='ui-grid-canvas']/div[1]/div[1]/div[1]/div[1]")).click();
@@ -111,61 +123,68 @@ public class AVIS_GUI_Manifest {
 					String ResNo1= ResNo.getText();
 					System.out.println(ResNo.getText());	
 	//		Thread.sleep(2000);
-					rwe.setCellData("INPUT_DATA", k, 16, ResNo1);
+					rwe.setCellData("Manifest_Avis", k, 15, ResNo1);
 					
 					WebElement AWD = driver.findElement(By.xpath("//*[@id='resDetailPanel']/div[2]/div[2]/div[1]/div/div[3]/div[2]"));
 					String AWD1= AWD.getText();
 					System.out.println(AWD.getText());	
-					rwe.setCellData("INPUT_DATA", k, 18, AWD1);
+					rwe.setCellData("Manifest_Avis", k, 17, AWD1);
 					
 					WebElement Wizard = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[1]/div[1]/div/div[5]/div[2]"));
 					Wizard.getText();
 					String Wizard1 = Wizard.getText();
 					System.out.println(Wizard.getText());	
-					rwe.setCellData("INPUT_DATA", k, 19, Wizard1);
+					rwe.setCellData("Manifest_Avis", k, 18, Wizard1);
 					
 					WebElement Coupon = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[2]/div[1]/div/div[7]/div[2]"));
 					Coupon.getText();
 					String Coupon1 = Coupon.getText();
 					System.out.println(Coupon.getText());	
-					rwe.setCellData("INPUT_DATA", k, 47, Coupon1);
+					rwe.setCellData("Manifest_Avis", k, 46, Coupon1);
 					
 					WebElement PickUpLoc = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[3]/div[3]/div/div[3]/div[2]/span"));
 					PickUpLoc.getText();
 					String PickUpLoc1 = PickUpLoc.getText();
 					System.out.println(PickUpLoc.getText());	
-					rwe.setCellData("INPUT_DATA", k, 20, PickUpLoc1);
+					rwe.setCellData("Manifest_Avis", k, 19, PickUpLoc1);
 					
 					WebElement PickupDate = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[3]/div[3]/div/div[5]/div[2]/span"));
 					PickupDate.getText();
 					String PickupDate1 = PickupDate.getText();
 					System.out.println(PickupDate.getText());	
-					rwe.setCellData("INPUT_DATA", k, 21, PickupDate1);
+					rwe.setCellData("Manifest_Avis", k, 20, PickupDate1);
 					
 					WebElement ReturnLoc = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[3]/div[5]/div/div[3]/div[2]/span"));
 					ReturnLoc.getText();
 					String ReturnLoc1 = ReturnLoc.getText();
 					System.out.println(ReturnLoc.getText());	
-					rwe.setCellData("INPUT_DATA", k, 22, ReturnLoc1);
+					rwe.setCellData("Manifest_Avis", k, 21, ReturnLoc1);
 					
 					WebElement ReturnDateTime = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[3]/div[5]/div/div[5]/div[2]/span"));
 					ReturnDateTime.getText();
 					String ReturnDateTime1 = ReturnDateTime.getText();
 					System.out.println(ReturnDateTime.getText());	
-					rwe.setCellData("INPUT_DATA", k, 23, ReturnDateTime1);
+					rwe.setCellData("Manifest_Avis", k, 22, ReturnDateTime1);
 					
 					WebElement CarClass = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[1]/div[2]/div/div[4]/span"));
 					CarClass.getText();
 					String CarClass1 = CarClass.getText();
 					System.out.println(CarClass.getText());	
-					rwe.setCellData("INPUT_DATA", k, 24, CarClass1);
+					rwe.setCellData("Manifest_Avis", k, 23, CarClass1);
 					
 					WebElement BookingDt = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[1]/div[2]/div/div[16]/span"));
 					BookingDt.getText();
 					String BookingDt1 = BookingDt.getText();
 					System.out.println(BookingDt.getText());	
-					rwe.setCellData("INPUT_DATA", k, 25, BookingDt1);
+					rwe.setCellData("Manifest_Avis", k, 24, BookingDt1);
 					Thread.sleep(2000);
+					
+	// Capture the screen shot of the report
+					Date d1= new Date();
+					SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+					File src1= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+					
+					FileUtils.copyFile(src1,new File( ScreenShotPath +TESTCASENAME+ sdf1.format(d1)+"_1Day"+".pgn"));
 					
 					//Select a row from the displayed open reservations
 					driver.findElement(By.id("mdetailCloseButton")).click();
@@ -177,7 +196,7 @@ public class AVIS_GUI_Manifest {
 					driver.findElement(By.xpath("//*[@id=\"manifestToDate\"]")).clear();
 					Thread.sleep(2000);
 					
-					String TwoDay_LOR = rwe.getCellData("INPUT_DATA", k, 13);
+					String TwoDay_LOR = rwe.getCellData("Manifest_Avis", k, 12);
 					driver.findElement(By.xpath("//*[@id=\"manifestToDate\"]")).sendKeys(TwoDay_LOR);
 					Thread.sleep(2000);
 					driver.findElement(By.xpath("//*[@id=\"getManifest\"]")).click();
@@ -193,7 +212,7 @@ public class AVIS_GUI_Manifest {
 					String max_str = max_TotItems1;
 					System.out.println(max_str);
 				    max_str = max_str.substring(9, max_str.length()-5);
-					rwe.setCellData("INPUT_DATA", k, 26, max_str);
+					rwe.setCellData("Manifest_Avis", k, 25, max_str);
 	
 	//Select a one of the record to open in report format		
 					driver.findElement(By.xpath("//div[@class='ui-grid-canvas']/div[1]/div[1]/div[1]/div[1]")).click();
@@ -204,18 +223,18 @@ public class AVIS_GUI_Manifest {
 					max_ResNo.getText();
 					String max_ResNo1= max_ResNo.getText();
 					System.out.println(max_ResNo.getText());
-					rwe.setCellData("INPUT_DATA", k, 27, max_ResNo1);
+					rwe.setCellData("Manifest_Avis", k, 26, max_ResNo1);
 					
 					WebElement max_AWD = driver.findElement(By.xpath("//*[@id='resDetailPanel']/div[2]/div[2]/div[1]/div/div[3]/div[2]"));
 					String max_AWD1= max_AWD.getText();
 					System.out.println(max_AWD.getText());	
-					rwe.setCellData("INPUT_DATA", k, 29, max_AWD1);
+					rwe.setCellData("Manifest_Avis", k, 28, max_AWD1);
 					
 					WebElement max_Wizard = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[1]/div[1]/div/div[5]/div[2]"));
 					max_Wizard.getText();
 					String max_Wizard1 = max_Wizard.getText();
 					System.out.println(max_Wizard.getText());	
-					rwe.setCellData("INPUT_DATA", k, 30, max_Wizard1);
+					rwe.setCellData("Manifest_Avis", k, 29, max_Wizard1);
 					
 					WebElement max_Coupon = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[2]/div[1]/div/div[7]/div[2]"));
 					max_Coupon.getText();
@@ -224,42 +243,48 @@ public class AVIS_GUI_Manifest {
 					max_PickUpLoc.getText();
 					String max_PickUpLoc1 = max_PickUpLoc.getText();
 					System.out.println(max_PickUpLoc.getText());	
-					rwe.setCellData("INPUT_DATA", k, 31, max_PickUpLoc1);
+					rwe.setCellData("Manifest_Avis", k, 30, max_PickUpLoc1);
 					
 					WebElement max_PickupDate = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[3]/div[3]/div/div[5]/div[2]/span"));
 					max_PickupDate.getText();
 					String max_PickupDate1 = max_PickupDate.getText();
 					System.out.println(max_PickupDate.getText());	
-					rwe.setCellData("INPUT_DATA", k, 32, max_PickupDate1);
+					rwe.setCellData("Manifest_Avis", k, 31, max_PickupDate1);
 					
 					WebElement max_ReturnLoc = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[3]/div[5]/div/div[3]/div[2]/span"));
 					ReturnLoc.getText();
 					String max_ReturnLoc1 = max_ReturnLoc.getText();
 					System.out.println(max_ReturnLoc.getText());	
-					rwe.setCellData("INPUT_DATA", k, 33, max_ReturnLoc1);
+					rwe.setCellData("Manifest_Avis", k, 32, max_ReturnLoc1);
 					
 					WebElement max_ReturnDateTime = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[3]/div[5]/div/div[5]/div[2]/span"));
 					max_ReturnDateTime.getText();
 					String max_ReturnDateTime1 = max_ReturnDateTime.getText();
 					System.out.println(max_ReturnDateTime.getText());	
-					rwe.setCellData("INPUT_DATA", k, 34, max_ReturnDateTime1);
+					rwe.setCellData("Manifest_Avis", k, 33, max_ReturnDateTime1);
 					
 					WebElement max_CarClass = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[1]/div[2]/div/div[4]/span"));
 					max_CarClass.getText();
 					String max_CarClass1 = max_CarClass.getText();
 					System.out.println(max_CarClass.getText());	
-					rwe.setCellData("INPUT_DATA", k, 35, max_CarClass1);
+					rwe.setCellData("Manifest_Avis", k, 34, max_CarClass1);
 					
 					WebElement max_BookingDt = driver.findElement(By.xpath("//*[@id=\"resDetailPanel\"]/div[2]/div[1]/div[2]/div/div[16]/span"));
 					max_BookingDt.getText();
 					String max_BookingDt1 = max_BookingDt.getText();
 					System.out.println(max_BookingDt.getText());
-					rwe.setCellData("INPUT_DATA", k, 36, max_BookingDt1);
+					rwe.setCellData("Manifest_Avis", k, 35, max_BookingDt1);
 					Thread.sleep(2000);
+					
+	// Capture the screen shot of the report
+					Date d2= new Date();
+					SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+					File src2= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+					FileUtils.copyFile(src2,new File( ScreenShotPath +TESTCASENAME+ sdf2.format(d2)+"_2Day"+".pgn"));
 					//Select a row from the displayed open reservations
 					driver.findElement(By.id("mdetailCloseButton")).click();
 					Thread.sleep(2000);
-			
+					
 	// Logout the application		
 					driver.findElement(By.xpath("//*[@id=\"headerLogOutButton\"]")).click();
 					Thread.sleep(2000);
